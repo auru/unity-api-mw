e06d29213010f854baa89beabeb1c4682ab7bb6c
Alexander Vakhitov <woodcutter@woodcutter-osx.local>
2018-07-25 23:07:54 +0500

    feat(response): add new middleware

---
+++ src/response.js
@@ -0,0 +1,19 @@
+import { isAPIError } from './utils';
+
+const defaultSettings = {
+    key: 'response'
+};
+
+export default function responseMw(settings) {
+    settings = Object.assign({}, defaultSettings, settings);
+
+    return next => async options => {
+        const result = await next();
+
+        if (isAPIError(result) || options[settings.key]) {
+            return result;
+        }
+
+        return result.body;
+    };
+}
